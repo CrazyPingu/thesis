4416fb807689bfbe14c9fbecc34bc6c4dacfd4ca
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-03-11 16:12:37 +0100

    Add changing favicon based on theme

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DynamicFavicon />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import DynamicFavicon from '@/components/DynamicFavicon.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    DynamicFavicon
   }
 }
 </script>
---
+++ src/components/DynamicFavicon.vue
@@ -0,0 +1,27 @@
+<template>
+    <link rel="icon" :href="faviconPath" />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isDarkMode: window.matchMedia('(prefers-color-scheme: dark)').matches,
+            faviconPath: '',
+        };
+    },
+    mounted() {
+        this.setFaviconPath();
+    },
+    methods: {
+        setFaviconPath() {
+            this.faviconPath = this.isDarkMode ? '/favicon-light.ico' : '/favicon-dark.ico';
+        },
+    },
+    watch: {
+        isDarkMode() {
+            this.setFaviconPath();
+        },
+    },
+};
+</script>
