def9b6cd260ecd912d0c189489b020f402fcee44
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-04-04 02:55:51 +0200

    changed from enum to dictionary

---
+++ src/backend/utils/dictionary_database.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This dictionary is used to map the table read from the xml file to the table in the database
+ *
+ * @package utils
+ *
+ */
+$database_table = array(
+  'Museo' => 'info_museo',
+  'Fermata_bus' => 'info_fermata',
+  'Percorso_escursionistico' => 'percorso_escursionistico',
+  'tipologia' => 'tipologia',
+  'Attrezzatura_alberghiera' => 'punto_di_interesse',
+  'Campeggio' => 'punto_di_interesse',
+  'Farmacia' => 'punto_di_interesse',
+  'Limitazione_al_transito' => 'punto_di_interesse',
+  'Ostello' => 'punto_di_interesse',
+  'Parcheggio' => 'punto_di_interesse',
+  'Pericolo_valanghe' => 'punto_di_interesse',
+  'Punto_panoramico_a_360_gradi' => 'punto_di_interesse',
+  'Punto_panoramico_orientato_a_Est' => 'punto_di_interesse',
+  'Punto_panoramico_orientato_a_Nord' => 'punto_di_interesse',
+  'Punto_panoramico_orientato_a_Ovest' => 'punto_di_interesse',
+  'Punto_panoramico_orientato_a_Sud' => 'punto_di_interesse',
+  'Ricovero_di_emergenza' => 'punto_di_interesse',
+  'Scuderia' => 'punto_di_interesse',
+  'Sorgente_o_fontana' => 'punto_di_interesse',
+  'Stazione_ferroviaria' => 'punto_di_interesse'
+);
+
+
+?>
