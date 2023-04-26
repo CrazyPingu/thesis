498c29cf8aef6f68f23358db5b2e4929946ccad8
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-04-26 03:52:01 +0200

    Adding basic line in the map

--- src/views/MappaEmiliaRomagna.vue
+++ src/views/MappaEmiliaRomagna.vue
@@ -2,18 +2,23 @@
   <h1>Mappa dell'Emilia Romagna con punti di interesse</h1>
   <div class="map">
     <l-map :zoom="zoom" :center="center">
-      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-tile-layer :url="url" :attribution="attribution" />
       <l-marker :lat-lng="markerLatLng">
         <l-popup>{{ markerPopup }} <br> haa</l-popup>
       </l-marker>
-      <percorso></percorso>
+      <!-- <percorso></percorso> -->
+      <!-- <div v-if="polygon.length > 0"> -->
+        <l-polyline :lat-lngs="polygon.latlngs" :color="polygon.color" />
+      <!-- </div> -->
+      <!-- <l-polygon :lat-lngs="polygon.latlngs" :color="polygon.color" /> -->
     </l-map>
   </div>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
-import Percorso from "@/components/MappaPercorso.vue";
+// import { LMap, LPolygon, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LMarker, LPopup, LPolyline } from "@vue-leaflet/vue-leaflet";
+// import Percorso from "@/components/MappaPercorso.vue";
 
 export default {
   name: "HomePage",
@@ -22,9 +27,11 @@ export default {
     LTileLayer,
     LMarker,
     LPopup,
-    Percorso,
+    LPolyline,
+    // Percorso,
   },
-  data() {
+  setup() {
+    console.log('setup');
     return {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
@@ -34,12 +41,20 @@ export default {
       center: [44.499211, 11.2492853],
       markerLatLng: [44.1481831, 12.2354155],
       markerPopup: 'Hello, this is a marker!',
+      // polygon: [],
+      polygon: {
+        latlngs: [
+          [44.5343768,10.5664061],
+          [45.2035418, 12.0933341],
+          [44.3188888,11.2002901],
+          [44.2035418, 12.0933341],
+        ],
+        color: 'red',
+      },
     };
-  }
+  },
 };
 </script>
-
-
 
 <style scoped>
 .map {
