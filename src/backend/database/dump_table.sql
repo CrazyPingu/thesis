e99764860bc90c4227d3d4790e919a209f8724fa
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-06-05 01:16:09 +0200

    Add table utente

--- src/backend/database/dump_table.sql
+++ src/backend/database/dump_table.sql
@@ -91,6 +91,17 @@ CREATE TABLE IF NOT EXISTS `percorso_escursionistico` (
   PRIMARY KEY (`idPercorso`),
   CONSTRAINT `percorso_escursionistico_ibfk_1` FOREIGN KEY (`idPercorso`) REFERENCES `identificatore` (`idPoi`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+-- -----------------------------------------------------
+-- Table `utenti`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `utente` (
+  `idUtente` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`idUtente`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
 ;
