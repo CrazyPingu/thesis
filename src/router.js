ecececfbe4ea5ab693d63d238d00e871f235d474
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-06-05 02:32:36 +0200

    Add both login and register page

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Aggiorna_poi from './views/Aggiorna_poi.vue';
+import LoginPage from './views/LoginPage.vue';
+import RegisterPage from './views/RegisterPage.vue';
 import Map from './views/Map.vue';
 
 const router = createRouter({
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/update-database',
       name: 'Update Database',
       component: Aggiorna_poi,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginPage,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: RegisterPage,
     },
   ],
 });
